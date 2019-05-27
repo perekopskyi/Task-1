310bd19b1fc7e91d68b97c113fff701d54472f66
Yevhenii <49187864+perekopskyi@users.noreply.github.com>
2019-05-27 13:36:09 +0300

    Исправил алгоритм
    1. Исправил ошибку при суммировании чисел ниже 0,01
    2. Поменял тип вводимых данных с text на number

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let input = document.querySelectorAll('input'),
 function sum(a, b) {
   a = +input[0].value; 
   b = +input[1].value;
-  result = (a * 10 + b * 10) / 10;
+  result = (a * 1000000000 + b * 1000000000) / 1000000000;
   return result;
 }
 
